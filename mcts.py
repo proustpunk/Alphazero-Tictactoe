61af86608e8c1dc276dce1c760c9218d1ee27e6b
proustpunk <sabinpyakurel0054@gmail.com>
2026-07-03 08:45:39 +0545

    Write select & expand logic for MCTS

--- mcts.py
+++ mcts.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-
+import math
 
 
 class Node:
@@ -25,15 +25,38 @@ root_board  = [
                 ['.', '.', '.']
         ]
 root_node = Node(root_board, None, root_player_symbol)
-def create_node(node, depth):
 
-    if depth == 0:
-        return
+child_node = []
+
+
+def ucb1(node):
+    if node.total_visit == 0:
+        return float('inf')
+    return (node.total_reward / node.total_visit) + 100 * math.sqrt(math.log(node.parent.total_visit) / node.total_visit)
+
+
+def select(node):
+    if node.children:
+        best_child = None
+        max_ucb = -float('inf')
+
+        for child in node.children:
+            this_ucb = ucb1(child)
+            if this_ucb > max_ucb:
+                max_ucb = this_ucb
+                best_child = child
+
+        return select(best_child)
+    else:
+        return expand(node)
+
+
+def expand(node):
     for i in range(3):
         for j in range(3):
 
             if node.board[i][j] == '.':
-                
+
                 new_parent = node
                 if node.player_symbol == 'o':
                     new_player_symbol = 'x'
@@ -41,26 +64,62 @@ def create_node(node, depth):
                     new_player_symbol = 'o'
 
                 new_board = deepcopy(node.board)
-
                 new_board[i][j] = new_player_symbol
 
                 new_child = Node(new_board, new_parent, new_player_symbol)
+                node.children.append(new_child)
 
-                node.children.append(new_child)   
-
-                create_node(new_child, depth-1)
-
-
-create_node(root_node, 5)
-
-def print_node(node, depth=0):
-    indent = "  " * depth
-    print(f"{indent}player_symbol={node.player_symbol}")
-    for row in node.board:
-        print(indent + " ".join(row))
-    print()
-    for child in node.children:
-        print_node(child, depth + 1)
+                return new_child
 
 
-print_node(root_node)
+
+
+
+
+
+
+
+
+
+
+
+
+# def create_node(node, depth):
+
+#     if depth == 0:
+#         return
+#     for i in range(3):
+#         for j in range(3):
+
+#             if node.board[i][j] == '.':
+                
+#                 new_parent = node
+#                 if node.player_symbol == 'o':
+#                     new_player_symbol = 'x'
+#                 elif node.player_symbol == 'x':
+#                     new_player_symbol = 'o'
+
+#                 new_board = deepcopy(node.board)
+
+#                 new_board[i][j] = new_player_symbol
+
+#                 new_child = Node(new_board, new_parent, new_player_symbol)
+
+#                 node.children.append(new_child)   
+
+#                 create_node(new_child, depth-1)
+
+
+# create_node(root_node, 5)
+
+# def print_node(node, depth=0):
+#     indent = "  " * depth
+#     print(f"{indent}player_symbol={node.player_symbol}")
+#     for row in node.board:
+#         print(indent + " ".join(row))
+#     print()
+#     for child in node.children:
+#         print_node(child, depth + 1)
+
+
+# print_node(root_node)
